62867a512230a90064d79fa40cbae749e50b284f
New_App <bason1607@gmail.com>
2024-11-14 12:32:17 +0700

    signup user in be ok

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const cors = require("cors");
+const authRouter = require("./src/routers/authRouter");
+const connectDB = require("./src/configs/connectDb");
+const errorMiddleHandle = require("./src/middlewares/errorMiddleware");
+require('dotenv').config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-const PORT = 3001;
+const PORT = 3001;    
 
-app.get('/auth/hello', (_req, res) =>{
-    res.send('<h1>Hello world</h1>')
-})
+app.use('/auth', authRouter);
+
+connectDB();
+
+app.use(errorMiddleHandle)
 
 app.listen(PORT, (err) => {
   if (err) {
